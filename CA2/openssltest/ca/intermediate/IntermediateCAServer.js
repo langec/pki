3d494f74e8217f36507ba8091b1364bfd5b1a6d1
Philipp M <M1ll3@web.de>
2015-06-05 10:27:21 +0200

    ca fertig

--- CA2/openssltest/ca/intermediate/IntermediateCAServer.js
+++ CA2/openssltest/ca/intermediate/IntermediateCAServer.js
@@ -10,9 +10,9 @@ function handleError(res, err)
     if(err)
     {
         console.log(err);
-        res.header("Status Code", "500");
-        res.header("Content-type", "text/plain");
-        res.send(err);
+        res.writeHead(500, {"Content-type" : "text/plain"});
+        //res.header("Content-type", "text/plain");
+        res.end(err);
     }
 }
 
@@ -27,8 +27,8 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
     console.log("Sending help response.");
-    res.header("Content-type", "text/plain");
-    res.send("To issue a certificate request: POST to /certificateRequests");
+    res.writeHead(200, {"Content-type" : "text/plain"});
+    res.end("To issue a certificate request: POST to /certificateRequests");
 });
 
 app.post('/certificateRequests', function (req, res) {
@@ -39,39 +39,51 @@ app.post('/certificateRequests', function (req, res) {
     var csrName = timestamp + ".csr.pem";
     
     //write the request to a tmp file
+    console.log("writing tmp csr");
     fs.writeFileSync("csr/" + csrName, req.body);
     
     var certName = timestamp + ".cert.pem";
     
     //sign request using openssl
+    console.log("signing tmp csr");
     child = exec("openssl ca -batch -config openssl.cnf -extensions server_cert -notext -md sha256 -in csr/"+csrName+" -out certs/"+certName+" -key dieme1234", function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
-        if (error !== null) {
+        if(stderr.indexOf("TXT_DB error number 2") >= 0)
+        {
+            handleError(res, "This certificate request was already signed.");
+        }
+        else if (error !== null) {
+            console.log("error while signing csr");
             handleError(res, error);
         }
         else
         {
+            console.log("verifying cert against ca chain");
             //verify cert against ca chain
-            child = exec("openssl verify -CAfile /certs/ca-chain.cert.pem certs/"+certName, function (error, stdout, stderr) {
+            child = exec("openssl verify -CAfile certs/ca-chain.cert.pem certs/"+certName, function (error, stdout, stderr) {
                 console.log('stdout: ' + stdout);
                 console.log('stderr: ' + stderr);
                 if (error !== null) 
                 {
+                    console.log("error while verifying cert against ca chain");
                     handleError(res, error);
                 } 
-                else if(stdout != "OK")
+                else if(stdout !== "certs/"+certName + ": OK\n")
                 {
+                    console.log("NOT OK: verifying cert against ca chain");
                     handleError(res, "Certificate validation against ca chain failed. (Was: "+stdout+")");
                 } 
                 else
                 {
                     //send cert back to client
+                    console.log("reading written cert");
                     var certContent = fs.readFileSync("certs/"+certName);
                     
                     res.header("Status Code", "200");
                     res.header("Content-type", "text/plain");
-                    res.send(certContent);
+                    console.log("sending cert");
+                    res.end(certContent);
                 }
             });
         }

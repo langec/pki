9afa33b6a911119f182b5ed5f8d8582305afac6a
Christian Lange <christian.lange@firemail.de>
2015-06-19 09:23:14 +0200

    call ca webservice++

--- validation/validation.js
+++ validation/validation.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 var mongojs = require('mongojs');
-		  var db = mongojs('registration',['requests']);
+var db = mongojs('registration',['requests']);
+var http = require('http');
 
 
 
@@ -14,7 +15,6 @@ app.get('/', function (req, res) {
    db.requests.find({status:'new' },function(err, docs) {
     // docs is an array of all the documents in mycollection
     var data = docs;
-    console.log(data);
     res.send(data);
   });
   
@@ -30,19 +30,20 @@ app.post('/approve', function (req, res) {
   }, function(err, doc, lastErrorObject) {
       csrRequest = doc;
       console.log(doc.cn);
-      console.log("openssl req -new -config etc/server.conf -out csrs/"+csrRequest.cn+".csr -keyout pkeys/"+csrRequest.cn+".key -subj /C="+csrRequest.c+"/ST=\""+csrRequest.st+"\"/L=\""+csrRequest.l+"\"/O=\""+csrRequest.o+"\"/OU=\""+csrRequest.ou+"\"/CN=\""+csrRequest.cn+"\"");
       //cert req erstellen
       var exec = require('child_process').exec;
       //openssl req -new -config etc/server.conf -out certs/simple.org.csr -keyout certs/simple.org.key
       var child = exec("openssl req -new -config etc/server.conf -out csrs/"+csrRequest.cn+".csr -keyout pkeys/"+csrRequest.cn+".key -subj /C="+csrRequest.c+"/ST=\""+csrRequest.st+"\"/L=\""+csrRequest.l+"\"/O=\""+csrRequest.o+"\"/OU=\""+csrRequest.ou+"\"/CN=\""+csrRequest.cn+"\"",
         function (error, stdout, stderr) {
+          console.log("exec done");
           if(error){
-            console.log(error);
+            console.log("error" + error);
             console.log(stderr);
           }
           
           console.log("reading csr file: " + "./csrs/"+csrRequest.cn+".csr")
-          fs.readFileSync("./csrs/"+csrRequest.cn+".csr",'utf-8',function(err,data){
+          fs.readFile("./csrs/"+csrRequest.cn+".csr",'utf-8',function(err,data){
+            console.log(err);
             if(!err){
               console.log("postToCa");
               postToCa(data);
@@ -57,23 +58,8 @@ app.post('/approve', function (req, res) {
   });
 });
 
-function postToCa(data){
-  console.log("posting csr to ca");
-  var caPostOptions = {
-    hostname: 'www.google.com',
-    port: 80,
-    path: '/upload',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': data.length
-    }
-  };
-  console.log(data);
-}
 
 app.post('/reject', function (req, res) {
-  console.log(req.body);
   //registration request löschen oder auf abgelehnt/angenommen setzen
   db.requests.findAndModify({
       query: { _id: mongojs.ObjectId(req.body.id) },
@@ -89,6 +75,32 @@ app.post('/reject', function (req, res) {
   res.send("request rejected");
 });
 
+function postToCa(data){
+  console.log("posting csr to ca");
+  var caPostOptions = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/certificateRequests',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text',
+      'Content-Length': data.length
+    }
+  };
+  console.log(data);
+  var req = http.request(caPostOptions,function(res){
+    console.log("response ca: " + res.statusCode);
+    res.on('data', function(chunk){
+      console.log("chunk: "+ chunk);
+    });
+    req.on('error', function(e){
+      console.log("error: " + e);
+    });
+  })
+  req.write(data);
+  req.end();
+}
+
 var server = app.listen(4400, function () {
 
   var host = server.address().address;
@@ -97,3 +109,4 @@ var server = app.listen(4400, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
+

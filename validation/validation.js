fb345f336fcce478e53e8f5a90757837af034592
Christian Lange <christian.lange@firemail.de>
2015-07-27 13:38:50 +0200

    2.

--- validation/validation.js
+++ validation/validation.js
@@ -198,7 +198,7 @@ function postToCa(request,data){
   var req = http.request(caPostOptions,function(res){
     //antwort der ca 
     console.log("response ca: " + res.statusCode);
-    var caResponse;
+    var caResponse = '';
     //ca schickt daten, kann in mehreren blöcken geschehen(theoretisch)
     var data;
     res.on('data', function(chunk){
